b89321e8dab0bf23df3f5654b989858d38627836
Alpha Olomi <alphaolomi@gmail.com>
2021-05-09 09:29:49 +0300

    feat: ui tweak

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home(props) {
     });
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="min-h-screen overflow-auto ">
       <NextSeo
         title="Inspire Me Now"
         description="Exceptional, curated qoutes to lift your day"
@@ -33,52 +33,57 @@ export default function Home(props) {
 
       <ToastContainer />
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <CopyToClipboard
-          onCopy={notify}
-          text={`"${props.quote.text}" - ${props.quote.author}`}
-        >
-          <blockquote className="text-6xl font-bold">
-            {props.quote.text}
-            <br />-
-            <cite className="text-blue-600 italic">{props.quote.author}</cite>
-          </blockquote>
-        </CopyToClipboard>
-      </main>
+      <div className="container">
+        <main className="flex flex-col h-screen justify-between px-4">
+          <div className="flex flex-col  items-center justify-center flex-1 text-center">
+            <CopyToClipboard
+              onCopy={notify}
+              text={`"${props.quote.text}" - ${props.quote.author}`}
+            >
+              <blockquote className="text-6xl font-bold ">
+                {props.quote.text}
+                <br />-
+                <cite className="text-blue-600 italic">
+                  {props.quote.author}
+                </cite>
+              </blockquote>
+            </CopyToClipboard>
+          </div>
 
-      <footer className="flex justify-center h-24 w-full space-x-3 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://github.com/alphaolomi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made with ❤️ by @alphaolomi {"|"}
-        </a>
-        <a
-          className="flex items-center justify-center"
-          href="https://github.com/dwyl/quotes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by dwyl/quotes {"| "}
-        </a>
-        <a
-          className="flex items-center justify-center"
-          href="https://github.com/alphaolomi/inspire-me-now"
-        >
-          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Finspire-me-now.vercel.app&count_bg=%23CFCFCF&title_bg=%23A8A8A8&icon=&icon_color=%23E7E7E7&title=inspired&edge_flat=true" 
-            alt="inspired tag" 
-            width="120" 
-            height="15" 
- 
-          />
-        </a>
-      </footer>
+          <footer className="h-24 md:h-1/6  w-full flex justify-around lg:justify-center lg:gap-5 gap-1   border-t">
+            <a
+              className="flex items-center justify-center"
+              href="https://github.com/alphaolomi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Made with ❤️ by @alphaolomi
+            </a>
+            <a
+              className="flex items-center justify-center"
+              href="https://github.com/dwyl/quotes"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by dwyl/quotes
+            </a>
+            <a
+              className="flex items-center justify-center"
+              href="https://github.com/alphaolomi/inspire-me-now"
+            >
+              <img
+                src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Finspire-me-now.vercel.app&count_bg=%23CFCFCF&title_bg=%23A8A8A8&icon=&icon_color=%23E7E7E7&title=inspired&edge_flat=true"
+                alt="inspired tag"
+                width="120"
+                height="15"
+              />
+            </a>
+          </footer>
+        </main>
+      </div>
     </div>
   );
 }
-
 
 export async function getServerSideProps(context) {
   const quote = quotes.random();
